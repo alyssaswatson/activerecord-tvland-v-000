89f4c269929a9c010d713518d7b8780bcc6359d9
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-02-14 00:54:26 +0000

    automatically backed up by learn

--- db/migrate/006_add_actor_and_catchphrase_to_characters.rb
+++ db/migrate/006_add_actor_and_catchphrase_to_characters.rb
@@ -3,4 +3,4 @@ class AddActorAndCatchphraseToCharacters < ActiveRecord::Migration[4.2]
     add_column :characters, :actor_id, :integer
     add_column :characters, :catchphrase, :string
   end
-  End
+end
